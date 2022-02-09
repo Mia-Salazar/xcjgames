4153facfd910902716c7504fc36ed2340a650bd1
Mia-Salazar <maria.sgr@gmail.com>
2022-02-09 20:19:46 +0100

    Add sudo comand

--- src/app/utils/middleCheck.js
+++ src/app/utils/middleCheck.js
@@ -1,5 +1,5 @@
 export const middleCheck = (route) => {
-    const external = ["portfolio", "professional", "profesional", "personal"];
+    const external = ["portfolio", "professional", "profesional", "personal", "sudo"];
     const externalFound = external.indexOf(route);
     if (externalFound !== -1) {
         return true;
