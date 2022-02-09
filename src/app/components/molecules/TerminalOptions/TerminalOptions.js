bd610986f882b293abee7d5d2aafcaa345d98c99
Mia-Salazar <maria.sgr@gmail.com>
2022-02-09 18:50:29 +0100

    Add external links to linkedin, twitter, github and email

--- src/app/components/molecules/TerminalOptions/TerminalOptions.js
+++ src/app/components/molecules/TerminalOptions/TerminalOptions.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 import "./TerminalOptions.scss";
+import { ActionChecker } from "../../../utils/actionChecker";
 
 export const TerminalOptions = ({completeText, resetTextStatus, setLanguage}) => {
     const [data, setData] = useState("");
@@ -11,10 +12,10 @@ export const TerminalOptions = ({completeText, resetTextStatus, setLanguage}) =>
         resetTextStatus();
         event.preventDefault();
         if (data === "english" || data === "spanish") {
-            const newLanguage = data === 'spanish' ? 'es' : 'en';
+            const newLanguage = data === "spanish" ? "es" : "en";
             setLanguage(newLanguage);
-        } else if (data !== "") {
-            navigate(`/${data}`);
+        } else {
+            ActionChecker(data, navigate);
         }
         setData("");
         event.target.reset();
@@ -34,7 +35,7 @@ export const TerminalOptions = ({completeText, resetTextStatus, setLanguage}) =>
 	return (
         <footer className="terminal-options">
             <form className="terminal-options__form" onSubmit={handleSubmit}>
-                <label className="terminal-options__label" htmlFor="action"> visitor<span className="terminal-options__greater">{'>'}</span> </label>
+                <label className="terminal-options__label" htmlFor="action"> visitor<span className="terminal-options__greater">{">"}</span> </label>
                 <input className="terminal-options__input" type="text" id="action"
                          name="action" autoFocus onChange={handleInputChange} onKeyDown={handleKeyDown}/>
             </form>
---
+++ src/app/utils/actionChecker.js
@@ -0,0 +1,13 @@
+import { redirectTo }  from "./redirectTo";
+
+export const ActionChecker = (data, navigate) => {
+    const external = ["email", "linkedin", "twitter", "github"];
+    if (data !== "") {
+        const externalFound = external.indexOf(data);
+        if (externalFound !== -1) {
+            redirectTo(data);
+        } else {
+            navigate(`/${data}`);
+        }
+    }
+}
