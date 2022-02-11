1976c4a5aea61c3d6d23a65da60c4c634b9f9c13
Mia-Salazar <maria.sgr@gmail.com>
2022-02-11 18:27:57 +0100

    Fix scroll

--- src/app/pages/Home/Home.js
+++ src/app/pages/Home/Home.js
@@ -11,6 +11,13 @@ export const Home = ({completeState, language}) => {
 		<>
 			<section className="home">
 				<ConsoleText text="home.title" complete={completeState} language={language} />
+				<ConsoleText text="home.title" complete={completeState} language={language} />
+				<ConsoleText text="home.title" complete={completeState} language={language} />
+				<ConsoleText text="home.title" complete={completeState} language={language} />
+				<ConsoleText text="home.title" complete={completeState} language={language} />
+				<ConsoleText text="home.title" complete={completeState} language={language} />
+				<ConsoleText text="home.title" complete={completeState} language={language} />
+				<ConsoleText text="home.title" complete={completeState} language={language} />
 				<figure className="home__figure">
 					<img className="home__image" alt="" src={Logo}/>
 				</figure>
