303caeacdbd45427658c7fa39193a452c0d5ef8b
Mia-Salazar <maria.sgr@gmail.com>
2022-02-12 14:45:07 +0100

    Add current page

--- src/app/utils/middleCheck.js
+++ src/app/utils/middleCheck.js
@@ -1,5 +1,5 @@
 export const middleCheck = (route) => {
-    const external = ["portfolio", "professional", "profesional", "personal", "sudo", "i-am-your-father"];
+    const external = ["portfolio", "professional", "profesional", "personal", "sudo", "i-am-your-father", "current"];
     const externalFound = external.indexOf(route);
     if (externalFound !== -1) {
         return true;
