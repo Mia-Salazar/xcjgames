72605b541d675cfd26d49723e632c57b6c9ea30a
Mia-Salazar <maria.sgr@gmail.com>
2022-02-17 20:04:39 +0100

    Add more comments

--- src/app/utils/data/commandsList.js
+++ src/app/utils/data/commandsList.js
@@ -39,7 +39,7 @@ export const commandsListPriority = [
 
 //This array redirects to a new page with just i18n texts
 export const middleCheckList = ["portfolio", "professional", "profesional", "personal", "sudo", "i-am-your-father", "current", "error"];
-//This array redirects to a new page i18n texts and ASCII image
+//This array redirects to a new page with i18n texts and a ASCII image
 //To add a new image, add it to imageForProject.js and with the same name as the command
 export const projectsList = ["boreal", "padaone", "metal", "planet", "treeventure", "damng", "dielucard", "fire", "oriadris",];
 //This array is for commands that redirects to outside pages
